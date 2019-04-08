f6677035f3a3692d66563f5e0793e52161bdd5e1
Widjajat <widjajat@student.unimelb.edu.au>
2019-04-08 22:05:33 +1000

    Skills schema

--- server/models/Skills.js
+++ server/models/Skills.js
@@ -3,6 +3,9 @@ var mongoose = require('mongoose');
 // TODO: add details to the schema
 var skillSchema = mongoose.Schema(
     {
+        "Name":String, 
+        "Description":String, 
+        "Category":String 
 
     }
 );
