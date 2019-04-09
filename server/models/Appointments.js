86c58ee3780abf26b1454ca22da30dd3dff70906
MauriceBernardo <maurice_tikus@hotmail.com>
2019-04-09 14:01:26 +1000

    Associate ObjectId with mongoose ObjectId

--- server/models/Appointments.js
+++ server/models/Appointments.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
+var ObjectId = mongoose.Schema.Types.ObjectId;
 
 // Details of the Appointments schema
 var appointmentSchema = mongoose.Schema({
--- server/models/Users.js
+++ server/models/Users.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
+var ObjectId = mongoose.Schema.Types.ObjectId;
 
 // Details of the Users schema
 var userSchema = mongoose.Schema({
