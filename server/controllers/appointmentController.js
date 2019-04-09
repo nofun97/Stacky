f80ba3ef02e3306546a4b044ad7e890f6136cda3
shaditio <stevenhaditio@yahoo.com>
2019-04-09 15:23:21 +1000

    added controller of Appointments

---
+++ server/controllers/appointmentController.js
@@ -0,0 +1,73 @@
+var mongoose = require("mongoose");
+
+// Require the model that's related for this controller
+var Appointments = mongoose.model("Appointments");
+
+// Define the function to be used (create, remove, update, findall, etc.)
+
+// Method to find all appointments
+var findAllAppointments = function(req, res) {
+  Appointments.find(function(err, appointment) {
+    if (!err) {
+      res.send(appointment);
+    } else {
+      res.sendStatus(404);
+    }
+  });
+};
+
+// Method to add new appointment
+var addNewAppointments = function(req, res) {
+  var appointment = new Appointments({
+    Time: req.body.Date,
+    Description: req.body.String,
+    Address: req.body.String,
+    Teacher: req.body.ObjectId,
+    Student: req.body.ObjectId,
+  });
+  skill.save(function(err, appointment) {
+    if (!err) {
+      res.send(appointment);
+    } else {
+      res.sendStatus(400);
+    }
+  });
+};
+
+// Delete an appointment
+var deleteAppointments = function(req,res){
+  Appointments.findByIdAndDelete(id, function(err){
+    if (!err) {
+      res.send({DeletionSuccessful : true});
+    } else {
+      res.sendStatus(400);
+    }
+  });
+    
+};
+
+// Update an appointment
+var updateAppointments = function(req,res){
+  Appointments.findByIdAndDelete(id,
+    {
+      Time: req.body.Date,
+      Description: req.body.String,
+      Address: req.body.String,
+      Teacher: req.body.ObjectId,
+      Student: req.body.ObjectId,
+    },
+    function(err){
+    if (!err) {
+      res.send({UpdateSuccessful : true});
+    } else {
+      res.sendStatus(400);
+    }
+  });
+    
+};
+
+// Export the variable
+module.exports.findAllAppointments= findAllAppointments;
+module.exports.addNewAppointments = addNewAppointments;
+module.exports.deleteAppointments = deleteAppointments;
+module.exports.updateAppointments = updateAppointments;
