6a95cec73d197e95bbad4bf0d61f20d3546f7213
shaditio <stevenhaditio@yahoo.com>
2019-04-09 15:57:52 +1000

    modified variables

--- server/controllers/appointmentController.js
+++ server/controllers/appointmentController.js
@@ -1,5 +1,9 @@
 var mongoose = require("mongoose");
+
+// Require the model that's related for this controller
 var Appointments = mongoose.model("Appointments");
+
+// Define the function to be used (create, remove, update, findall, etc.)
 
 // Method to find all appointments
 var findAllAppointments = function(req, res) {
@@ -15,11 +19,11 @@ var findAllAppointments = function(req, res) {
 // Method to add new appointment
 var addNewAppointments = function(req, res) {
   var appointment = new Appointments({
-    Time: req.body.Date,
+    Time: req.body.Time,
     Description: req.body.String,
     Address: req.body.String,
-    Teacher: req.body.ObjectId,
-    Student: req.body.ObjectId,
+    Teacher: req.body.Teacher,
+    Student: req.body.Student,
   });
   skill.save(function(err, appointment) {
     if (!err) {
@@ -31,36 +35,39 @@ var addNewAppointments = function(req, res) {
 };
 
 // Delete an appointment
-var deleteAppointments = function(req, res) {
-  Appointments.findByIdAndDelete(id, function(err) {
+var deleteAppointments = function(req,res){
+  Appointments.findByIdAndDelete(id, function(err){
     if (!err) {
-      res.send({ DeletionSuccessful: true });
+      res.send({DeletionSuccessful : true});
     } else {
       res.sendStatus(400);
     }
   });
+    
 };
 
 // Update an appointment
-var updateAppointments = function(req, res) {
+var updateAppointments = function(req,res){
   var appointment = {
-    Time: req.body.Date,
-    Description: req.body.String,
-    Address: req.body.String,
-    Teacher: req.body.ObjectId,
-    Student: req.body.ObjectId,
-  };
-  Appointments.findByIdAndUpdate(id, appointment, function(err) {
+      Time: req.body.Time,
+      Description: req.body.String,
+      Address: req.body.String,
+      Teacher: req.body.Teacher,
+      Student: req.body.Student,
+    };
+  Appointments.findByIdAndUpdate(id, appointment, function(err){
     if (!err) {
-      res.send({ UpdateSuccessful: true });
+      res.send({UpdateSuccessful : true});
     } else {
       res.sendStatus(400);
     }
   });
+    
 };
 
+
 // Export the variable
-module.exports.findAllAppointments = findAllAppointments;
+module.exports.findAllAppointments= findAllAppointments;
 module.exports.addNewAppointments = addNewAppointments;
 module.exports.deleteAppointments = deleteAppointments;
 module.exports.updateAppointments = updateAppointments;
