cc4d9d9b5a44d3dd938e85c2109deb2629459725
Widjajat <widjajat@student.unimelb.edu.au>
2019-04-10 19:01:03 +1000

    Added deleteSkill and updateSkill

--- server/controllers/skillController.js
+++ server/controllers/skillController.js
@@ -42,7 +42,46 @@ var searchSkill = function(req, res) {
   )
 }
 
+var deleteSkill = function(req, res){
+  var id = req.params.id; 
+  Skills.findByIdAndDelete(id, function(err){
+    if (!err){
+      res.send({status: "Successfully deleted skill."});
+    }
+    else{
+      console.log(err); 
+      res.sendStatus(400); 
+    }
+  }
+    )
+
+}
+
+var updateSkill = function(req, res){
+  var id = req.params.id; 
+  var skill = {
+    Name: req.body.Name,
+    Description: req.body.Description,
+    Category: req.body.Category,
+  }; 
+  var options = {omitUndefined: true}; 
+  Skills.findByIdAndUpdate(id, skill, options, function(err){
+    if (!err){
+      res.send({status: "Successfully updated skill."});
+    }
+    else{
+      console.log(err);
+      res.sendStatus(400);
+    }
+  })
+}
+
+//search skill by category
+
+
 // Export the variable
 module.exports.findAllSkills = findAllSkills;
 module.exports.addNewSkill = addNewSkill;
 module.exports.searchSkill = searchSkill;
+module.exports.deleteSkill = deleteSkill; 
+module.exports.updateSkill = updateSkill; 
