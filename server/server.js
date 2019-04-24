ef9f29dca32654abb775900230080317a8135f65
MauriceBernardo <maurice_tikus@hotmail.com>
2019-04-24 22:05:00 +1000

    fixed IMPORTANT problem on Production mode

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, "../client/build")));
 
 //build mode
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 app.listen(PORT, function() {
