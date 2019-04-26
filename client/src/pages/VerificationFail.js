ae21c165552d134ea45438f0aa1f88c0de9a86a8
shaditio <stevenhaditio@yahoo.com>
2019-04-26 13:52:06 +1000

    modified VerificationFail Page

--- client/src/pages/VerificationFail.js
+++ client/src/pages/VerificationFail.js
@@ -7,22 +7,22 @@ class VerificationFail extends Component {
     return (
       <div className={styles.VerificationFail}>
         <section className={styles.Main}>
-          <h1 className = {styles.header}>We're Sorry</h1>
+          <h1 className={styles.header}>We're Sorry</h1>
           <p className={styles.paragraph}>
             Unfortunately, you are not eligible to join the service at this
-            time.
-            <br /> These may be one of the reasons:
+            time. These may be one of the reasons:
           </p>
           <ul className={styles.Information}>
             <li>You are not yet 18 years old.</li>
             <li>You did not pass the background check</li>
           </ul>
           <p className={styles.paragraph}>
-            We encourage you to try again <br />
-            when you have fulfilled either or both criteria.
+            We encourage you to try again when you have fulfilled either or both
+            criteria.
           </p>
         </section>
         <aside className={styles.Illust} />
+        <aside className={styles.image} />
       </div>
     );
   }
