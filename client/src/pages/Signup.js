b31cc37eeb65bf59b3a8bdd483613a34369370ce
Novan <novan.allanadi1211@gmail.com>
2019-04-26 16:45:34 +1000

    backend works, always gets error

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -7,23 +7,70 @@ import { Redirect } from "react-router-dom";
 class Signup extends Component {
   constructor(props) {
     super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
+      email: "",
+      password: "",
+      FirstName: "",
+      LastName: "",
+      DOB: "",
+      UName: "",
+      IsVerified: false,
+      Address: "",
+      confirmPassword: "",
+      ID: "",
       InvalidInfo: false,
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.render = this.render.bind(this);
   }
 
-  handleSubmit() {
-    this.setState({
-      InvalidInfo: true,
-    });
+  handleSubmit(event) {
+    console.log("Submitting...");
+    console.log(this.state);
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    //TODO: put url in env?
+    fetch("http://localhost:5000/api/user",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        // body: JSON.stringify(this.state.Submission)
+        body: JSON.stringify({
+          user: {
+            email: this.state.email,
+            password: this.state.password
+          },
+          FirstName: this.state.FirstName,
+          LastName: this.state.LastName,
+          DOB: this.state.DOB,
+          UName: this.state.UName,
+          IsVerified: this.state.IsVerified,
+          Address: this.state.Address
+        })
+      })
+      .then(data => {
+        this.setState({ ID: data._id });
+        console.log("Submission successful!");
+        console.log(data);
+      })
+      .catch(err => {
+        console.log("Submission not succesful");
+        console.log(err);
+        this.setState({
+          InvalidInfo: true
+        });
+      })
   }
 
   render() {
     if (this.state.InvalidInfo === true) {
       return <Redirect to="/invalid_info" />;
     }
-
     return (
       <div className={styles.Signup}>
         <section className={styles.Main}>
@@ -31,26 +78,38 @@ class Signup extends Component {
             <h1 className={styles.header}>Sign up</h1>
           </div>
           {/* change with implementing formik later */}
-          <Form className={styles.form} onSubmit={this.handleSubmit}>
-            <Form.Group className={styles.email} controlId="formBasicEmail">
+          <Form noValidate className={styles.form} onSubmit={(e) => this.handleSubmit(e)}>
+            <Form.Group role="form" className={styles.email} controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control required type="email" placeholder="Enter email"
+                onChange={e => this.setState({ email: e.target.value })}
+                value={this.state.email || ''}
+              />
             </Form.Group>
 
             <Form.Group className={styles.name} controlId="formBasicName">
               <div>
                 <Form.Label>First Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter First Name" />
+                <Form.Control required type="text" placeholder="Enter First Name"
+                  onChange={e => this.setState({ FirstName: e.target.value })}
+                  value={this.state.FirstName || ''}
+                />
               </div>
               <div>
                 <Form.Label>Surname</Form.Label>
-                <Form.Control type="text" placeholder="Enter Surname" />
+                <Form.Control required type="text" placeholder="Enter Surname"
+                  onChange={e => this.setState({ LastName: e.target.value })}
+                  value={this.state.LastName || ''}
+                />
               </div>
             </Form.Group>
 
             <Form.Group className={styles.dob} controlId="formBasicDOB">
               <Form.Label>Date of Birth</Form.Label>
-              <Form.Control className={styles.dateInput} type="date" placeholder="Enter Date of birth" />
+              <Form.Control required className={styles.dateInput} type="date" placeholder="Enter Date of birth"
+                onChange={e => this.setState({ DOB: e.target.value })}
+                value={this.state.DOB}
+              />
             </Form.Group>
 
             <Form.Group
@@ -58,7 +117,10 @@ class Signup extends Component {
               controlId="formBasicPassword"
             >
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control required type="password" placeholder="Password"
+                onChange={e => this.setState({ password: e.target.value })}
+                value={this.state.password || ''}
+              />
             </Form.Group>
 
             <Form.Group
@@ -66,7 +128,11 @@ class Signup extends Component {
               controlId="formBasicConfirmPassword"
             >
               <Form.Label>Confirm Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control required type="password" placeholder="Password"
+                onChange={e => this.setState({ confirmPassword: e.target.value })}
+                value={this.state.confirmPassword || ''}
+                isInvalid={!(this.state.confirmPassword === this.state.password)}
+              />
             </Form.Group>
 
             <Form.Group className={styles.doc} controlId="formBasicDocuments">
