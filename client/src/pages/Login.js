3b90cc82964145104cf76caa23204c2a2b9d243b
Novan <novan.allanadi1211@gmail.com>
2019-04-27 13:26:36 +1000

    added working login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,6 +4,48 @@ import Button from 'react-bootstrap/Button';
 import styles from "../styles/pages/Login.module.css";
 
 class Login extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    }
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleLogin(event) {
+    console.log("Login...");
+    console.log(this.state);
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    //TODO: put url in env?
+    fetch("http://localhost:5000/api/login",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          email: this.state.email,
+          password: this.state.password
+        }
+      })
+    })
+    .then(data => {
+      console.log("Login successful!");
+      console.log(data);
+    })
+    .catch(err => {
+      console.log("Login not succesful");
+      console.log(err);
+    })
+  }
+
   render() {
     return (
       <div className={styles.Login}>
@@ -13,10 +55,13 @@ class Login extends Component {
             <h2 className={styles.subheader}>Log in</h2>
           </div>
           {/* change with implementing formik later */}
-          <Form className={styles.form}>
+          <Form noValidate className={styles.form} onSubmit={this.handleLogin}>
             <Form.Group className={styles.email} controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control required type="email" placeholder="Enter email" 
+                onChange={e => this.setState({ email: e.target.value })}
+                value={this.state.email || ''}
+              />
               <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
               </Form.Text>
@@ -24,7 +69,10 @@ class Login extends Component {
 
             <Form.Group className={styles.password} controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control required type="password" placeholder="Password" 
+                onChange={e => this.setState({ password: e.target.value })}
+                value={this.state.password || ''}
+              />
             </Form.Group>
             <Button className={styles["btn-secondary"]} variant="primary" type="submit">
               Submit
