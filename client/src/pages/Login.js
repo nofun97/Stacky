fff06abe4be98cbb99c591554786c415da015619
MauriceBernardo <maurice_tikus@hotmail.com>
2019-04-27 14:02:26 +1000

    updated login navigation

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,49 +1,52 @@
 import React, { Component } from "react";
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import styles from "../styles/pages/Login.module.css";
 
 class Login extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       email: "",
-      password: ""
-    }
+      password: "",
+    };
     this.handleLogin = this.handleLogin.bind(this);
   }
 
   handleLogin(event) {
-    console.log("Login...");
-    console.log(this.state);
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
+      console.log("Login...");
+      console.log(this.state);
+      //TODO: put url in env?
+      fetch("http://localhost:5000/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user: {
+            email: this.state.email,
+            password: this.state.password,
+          },
+        }),
+      })
+        .then(data => {
+          console.log("Login successful!");
+          console.log(data);
+          // For authentication stuff (if ok then redirect) for now just redirect
+          // if (data.ok === true) {
+          // }
+          this.props.history.push("/home");
+        })
+        .catch(err => {
+          console.log("Login not succesful");
+          console.log(err);
+        });
     }
 
-    //TODO: put url in env?
-    fetch("http://localhost:5000/api/login",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        user: {
-          email: this.state.email,
-          password: this.state.password
-        }
-      })
-    })
-    .then(data => {
-      console.log("Login successful!");
-      console.log(data);
-    })
-    .catch(err => {
-      console.log("Login not succesful");
-      console.log(err);
-    })
+    event.preventDefault();
+    event.stopPropagation();
   }
 
   render() {
@@ -58,23 +61,36 @@ class Login extends Component {
           <Form noValidate className={styles.form} onSubmit={this.handleLogin}>
             <Form.Group className={styles.email} controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control required type="email" placeholder="Enter email" 
+              <Form.Control
+                required
+                type="email"
+                placeholder="Enter email"
                 onChange={e => this.setState({ email: e.target.value })}
-                value={this.state.email || ''}
+                value={this.state.email || ""}
               />
               <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
               </Form.Text>
             </Form.Group>
 
-            <Form.Group className={styles.password} controlId="formBasicPassword">
+            <Form.Group
+              className={styles.password}
+              controlId="formBasicPassword"
+            >
               <Form.Label>Password</Form.Label>
-              <Form.Control required type="password" placeholder="Password" 
+              <Form.Control
+                required
+                type="password"
+                placeholder="Password"
                 onChange={e => this.setState({ password: e.target.value })}
-                value={this.state.password || ''}
+                value={this.state.password || ""}
               />
             </Form.Group>
-            <Button className={styles["btn-secondary"]} variant="primary" type="submit">
+            <Button
+              className={styles["btn-secondary"]}
+              variant="primary"
+              type="submit"
+            >
               Submit
             </Button>
           </Form>
