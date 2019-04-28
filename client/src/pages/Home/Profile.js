d40631a4b53b710731a7fd05a776b96a48d1e581
Novan <novan.allanadi1211@gmail.com>
2019-04-28 15:41:31 +1000

    updated profile fetching

--- client/src/pages/Home/Profile.js
+++ client/src/pages/Home/Profile.js
@@ -10,9 +10,20 @@ class Profile extends Component {
   constructor(props) {
     super(props);
     this.handleEdit = this.handleEdit.bind(this);
-
+    this.handleFetch = this.handleFetch.bind(this);
     let description;
+    // setting state
+    this.state = {
+      editMode: false,
+      name: "",
+      email: "",
+      dateOfBirth: "",
+      interest: [],
+      skill: [],
+      description: "",
+    };
 
+    // if props is undefined, put placeholders
     if (this.props.location.state === undefined) {
       description = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
         facere dicta sapiente numquam voluptate iure deleniti veritatis odit
@@ -28,7 +39,11 @@ class Profile extends Component {
         description: description,
       };
     } else {
-      if (this.props.location.state.description === undefined) {
+      // if description is undefined put placeholders
+      if (
+        this.props.location.state.description === undefined ||
+        this.props.location.state.description === ""
+      ) {
         description = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
         facere dicta sapiente numquam voluptate iure deleniti veritatis odit
         veniam non nobis provident exercitationem autem, quam nesciunt
@@ -36,18 +51,73 @@ class Profile extends Component {
       } else {
         description = this.props.location.state.description;
       }
-      this.state = {
-        editMode: false,
-        name: `${this.props.location.state.LastName} ${
-          this.props.location.state.FirstName
-        }`,
-        email: this.props.location.state.email,
-        dateOfBirth: this.props.location.state.DOB,
-        interest: this.props.location.state.userInterest,
-        skill: this.props.location.state.userSkill,
-        description: description,
-      };
+
+      if (this.props.location.state.noBackend) {
+        this.state = {
+          editMode: false,
+          name: `${this.props.location.state.LastName} ${
+            this.props.location.state.FirstName
+          }`,
+          email: this.props.location.state.email,
+          dateOfBirth: this.props.location.state.DOB,
+          interest: this.props.location.state.userInterest,
+          skill: this.props.location.state.userSkill,
+          description: description,
+        };
+        console.log(this.state);
+      } else {
+        this.handleFetch(description);
+      }
     }
+  }
+
+  handleFetch(description) {
+    if (this.props.location.state.noBackend) return;
+    // fetching user data from express server
+    fetch("http://localhost:5000/api/credential", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        Credentials: this.props.id,
+      }),
+    })
+      .then(resp => {
+        console.log(resp);
+        return resp.json();
+      })
+      .then(data => {
+        var d = new Date(data.DOB);
+        this.setState({
+          // editMode: true,
+          name: data.FirstName + " " + data.LastName,
+          dateOfBirth:
+            d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate(),
+          interest: [
+            {
+              level: "Beginner",
+              value: "Algorithms",
+            },
+            {
+              level: "Advanced",
+              value: "Cooking",
+            },
+          ],
+          skill: [
+            {
+              level: "Advanced",
+              value: "Dancing",
+            },
+            {
+              level: "Intermediate",
+              value: "Data Structure",
+            },
+          ],
+          description: description,
+          email: this.props.location.state.email,
+        });
+      });
   }
 
   // handle if edit is clicked
