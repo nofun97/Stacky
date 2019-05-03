9ce2fd3b3e226752a76242e46a3bceae4e06b8ea
Novan <novan.allanadi1211@gmail.com>
2019-05-03 22:22:33 +1000

    add fetch users

--- client/src/pages/Home/Search.js
+++ client/src/pages/Home/Search.js
@@ -12,71 +12,147 @@ class Search extends Component {
     super(props);
     this.toggleDrawer = this.toggleDrawer.bind(this);
     this.handleSkillFilter = this.handleSkillFilter.bind(this);
+    this.handleFetchUsers = this.handleFetchUsers.bind(this);
     this.state = {
       user: [
-        {
-          firstName: "Frisco",
-          lastName: "Saol",
-          image: "holder.js/100px100",
-          skill: [{Name:"Baking", category:"Cooking", Description:"Nothing"},{Name:"Algorithm", category:"Computing", Description:"Nothing"}],
-          _id: "key",
-        },
-        {
-          firstName: "Frisco",
-          lastName: "Saol",
-          image: "holder.js/100px100",
-          skill: [],
-          _id: "key",
-        },
-        {
-          firstName: "Frisco",
-          lastName: "Saol",
-          image: "holder.js/100px100",
-          skill: [],
-          _id: "key",
-        },
-        {
-          firstName: "Frisco",
-          lastName: "Saol",
-          image: "holder.js/100px100",
-          skill: [],
-          _id: "key",
-        },
-        {
-          firstName: "Frisco",
-          lastName: "Saol",
-          image: "holder.js/100px100",
-          skill: [],
-          _id: "key",
-        },
-        {
-          firstName: "Frisco",
-          lastName: "Saol",
-          image: "holder.js/100px100",
-          skill: [],
-          _id: "key",
-        },
-        {
-          firstName: "Frisco",
-          lastName: "Saol",
-          image: "holder.js/100px100",
-          skill: [],
-          _id: "key",
-        },
-        {
-          firstName: "Frisco",
-          lastName: "Saol",
-          image: "holder.js/100px100",
-          skill: [],
-          _id: "key",
-        },
+        // {
+        //   firstName: "Frisco",
+        //   lastName: "Saol",
+        //   image: "holder.js/100px100",
+        //   skill: [{Name:"Baking", category:"Cooking", Description:"Nothing"},{Name:"Algorithm", category:"Computing", Description:"Nothing"}],
+        //   _id: "key",
+        // },
+        // {
+        //   firstName: "Frisco",
+        //   lastName: "Saol",
+        //   image: "holder.js/100px100",
+        //   skill: [],
+        //   _id: "key",
+        // },
+        // {
+        //   firstName: "Frisco",
+        //   lastName: "Saol",
+        //   image: "holder.js/100px100",
+        //   skill: [],
+        //   _id: "key",
+        // },
+        // {
+        //   firstName: "Frisco",
+        //   lastName: "Saol",
+        //   image: "holder.js/100px100",
+        //   skill: [],
+        //   _id: "key",
+        // },
+        // {
+        //   firstName: "Frisco",
+        //   lastName: "Saol",
+        //   image: "holder.js/100px100",
+        //   skill: [],
+        //   _id: "key",
+        // },
+        // {
+        //   firstName: "Frisco",
+        //   lastName: "Saol",
+        //   image: "holder.js/100px100",
+        //   skill: [],
+        //   _id: "key",
+        // },
+        // {
+        //   firstName: "Frisco",
+        //   lastName: "Saol",
+        //   image: "holder.js/100px100",
+        //   skill: [],
+        //   _id: "key",
+        // },
+        // {
+        //   firstName: "Frisco",
+        //   lastName: "Saol",
+        //   image: "holder.js/100px100",
+        //   skill: [],
+        //   _id: "key",
+        // },
       ],
       drawerOpen: false,
       pageNumber: 1,
       totalPageNumber: 1,
+      dataPerPage: 8,
+      currentIndex: 0,
+      totalItem: 0,
       filterSkill: [],
       skills: [],
     };
+  }
+
+  componentDidMount = async () => {this.handleFetchUsers()}
+
+  handleFetchUsers = async () => {
+    var query = `http://localhost:5000/api/users?from=${this.state.currentIndex}&size=${this.state.dataPerPage}`;
+    if (this.state.filterSkill.length > 0){
+      query += `&skills=${this.state.filterSkill.join(',')}`
+    }
+    const userData = await fetch(query);
+    const usersPage = await userData.json();
+    const users = usersPage.users;
+    const totalData = users.total;
+    var skillsObj = {};
+    var skillsList = [];
+    /*
+      user data list of 
+      {
+        firstName: string,
+        lastName: string,
+        image: string,
+        skill: []{Name: string, category: string, Description: string},
+        _id: string
+      }
+    */
+    var profiles = users.map(data => {
+      for (var i = 0; i < data.Skills.length; i++){
+        skillsObj[data.Skills[i].Skill] = 1
+      }
+      return {
+        firstName: data.FirstName,
+        lastName: data.LastName,
+        //TODO: change this
+        image: "holder.js/100px100",
+        skill: data.Skills,
+        _id: data._id
+      };
+    });
+
+    for (var s in skillsObj){
+      skillsList.push(s);
+    }
+
+    const skillsData = await fetch(`http://localhost:5000/api/skill?id=${skillsList.join(',')}`);
+    const skills = await skillsData.json();
+
+    profiles = profiles.map(data => {
+      let userSkills = [];
+      for (var i = 0; i < data.skill.length; i++) {
+        for (var j = 0; j < skills.length; j++){
+          if (data.skill[i].Skill === skills[j]._id) {
+            userSkills.push({
+              Name: skills[j].Name,
+              category: skills[j].Category,
+              Description: skills[j].Description
+            })
+            break;
+          }
+        }
+      }
+      console.log(data);
+      return {
+        firstName: data.firstName,
+        lastName: data.lastName,
+        //TODO: change this
+        image: data.image,
+        skill: userSkills,
+        _id: data._id
+      };
+    });
+    // console.log(users);
+    this.setState({user: profiles, totalItem: totalData/this.state.dataPerPage});
   }
 
   toggleDrawer = open => () => {
