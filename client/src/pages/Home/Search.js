4d6b4b587c7459520de4516f549cdc76cf552e73
MauriceBernardo <maurice_tikus@hotmail.com>
2019-05-04 13:14:14 +1000

    fixed search filter

--- client/src/pages/Home/Search.js
+++ client/src/pages/Home/Search.js
@@ -81,15 +81,29 @@ class Search extends Component {
       totalItem: 0,
       filterSkill: [],
       skills: [],
+      changes: false,
     };
   }
 
-  componentDidMount = async () => {this.handleFetchUsers()}
+  componentDidMount = async () => {
+    this.handleFetchUsers();
+  };
+
+  componentDidUpdate = async () => {
+    if (this.state.changes) {
+      this.handleFetchUsers();
+      this.setState({
+        changes: false,
+      })
+    }
+  };
 
   handleFetchUsers = async () => {
-    var query = `http://localhost:5000/api/users?from=${this.state.currentIndex}&size=${this.state.dataPerPage}`;
-    if (this.state.filterSkill.length > 0){
-      query += `&skills=${this.state.filterSkill.join(',')}`
+    var query = `http://localhost:5000/api/users?from=${
+      this.state.currentIndex
+    }&size=${this.state.dataPerPage}`;
+    if (this.state.filterSkill.length > 0) {
+      query += `&skills=${this.state.filterSkill.map(skill=> skill.id).join(",")}`;
     }
     const userData = await fetch(query);
     const usersPage = await userData.json();
@@ -108,8 +122,8 @@ class Search extends Component {
       }
     */
     var profiles = users.map(data => {
-      for (var i = 0; i < data.Skills.length; i++){
-        skillsObj[data.Skills[i].Skill] = 1
+      for (var i = 0; i < data.Skills.length; i++) {
+        skillsObj[data.Skills[i].Skill] = 1;
       }
       return {
         firstName: data.FirstName,
@@ -117,27 +131,29 @@ class Search extends Component {
         //TODO: change this
         image: "holder.js/100px100",
         skill: data.Skills,
-        _id: data._id
+        _id: data._id,
       };
     });
 
-    for (var s in skillsObj){
+    for (var s in skillsObj) {
       skillsList.push(s);
     }
 
-    const skillsData = await fetch(`http://localhost:5000/api/skill?id=${skillsList.join(',')}`);
+    const skillsData = await fetch(
+      `http://localhost:5000/api/skill?id=${skillsList.join(",")}`
+    );
     const skills = await skillsData.json();
 
     profiles = profiles.map(data => {
       let userSkills = [];
       for (var i = 0; i < data.skill.length; i++) {
-        for (var j = 0; j < skills.length; j++){
+        for (var j = 0; j < skills.length; j++) {
           if (data.skill[i].Skill === skills[j]._id) {
             userSkills.push({
               Name: skills[j].Name,
               category: skills[j].Category,
-              Description: skills[j].Description
-            })
+              Description: skills[j].Description,
+            });
             break;
           }
         }
@@ -149,12 +165,15 @@ class Search extends Component {
         //TODO: change this
         image: data.image,
         skill: userSkills,
-        _id: data._id
+        _id: data._id,
       };
     });
     // console.log(users);
-    this.setState({user: profiles, totalItem: totalData/this.state.dataPerPage});
-  }
+    this.setState({
+      user: profiles,
+      totalItem: totalData / this.state.dataPerPage,
+    });
+  };
 
   toggleDrawer = open => () => {
     this.setState({
@@ -162,17 +181,20 @@ class Search extends Component {
     });
   };
 
-  handleSkillFilter(filteredSkills){
+  handleSkillFilter(filteredSkills) {
     this.setState({
-      filterSkill:filteredSkills,
+      filterSkill: filteredSkills,
+      changes: true,
     });
   }
 
   handleNextPage = () => {
-    this.setState({currentIndex: this.state.currentIndex + this.state.dataPerPage + 1})
+    this.setState({
+      currentIndex: this.state.currentIndex + this.state.dataPerPage + 1,
+      changes: true,
+    });
     this.handleFetchUsers();
-  }
-
+  };
 
   render() {
     return (
@@ -192,7 +214,11 @@ class Search extends Component {
           onClose={this.toggleDrawer(false)}
         >
           {/* put the thing inside of the drawer here */}
-          <FilterDrawer filterSkill={this.state.filterSkill} skills={this.state.skills} onSkillFilter={this.handleSkillFilter}/>
+          <FilterDrawer
+            filterSkill={this.state.filterSkill}
+            skills={this.state.skills}
+            onSkillFilter={this.handleSkillFilter}
+          />
         </Drawer>
         <div className={styles["card-container"]}>
           <PeopleCardList values={this.state.user} />
@@ -202,7 +228,9 @@ class Search extends Component {
           <span className={styles.pagination}>
             page {this.state.pageNumber} of {this.state.totalPageNumber}
           </span>
-          <Button className={styles["next-btn"]} onClick={this.handleNextPage}>></Button>
+          <Button className={styles["next-btn"]} onClick={this.handleNextPage}>
+            >
+          </Button>
         </div>
       </section>
     );
