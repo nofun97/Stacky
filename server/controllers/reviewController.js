1630fc8e7bc26dbd1d65c517196907f038ac8bce
Novan <novan.allanadi1211@gmail.com>
2019-05-18 20:03:51 +1000

    appropriate controller and model for feedbacks

--- server/controllers/reviewController.js
+++ server/controllers/reviewController.js
@@ -2,15 +2,15 @@ var mongoose = require("mongoose");
 var Review = mongoose.model("Reviews");
 
 // Function to get all the review
-var getAllReview = function(req, res) {
-  Review.find(function(err, reviews) {
-    if (!err) {
-      res.send(reviews);
-    } else {
-      res.sendStatus(404);
-    }
-  });
-};
+// var getAllReview = function(req, res) {
+//   Review.find(function(err, reviews) {
+//     if (!err) {
+//       res.send(reviews);
+//     } else {
+//       res.sendStatus(404);
+//     }
+//   });
+// };
 
 // Function to add one review
 var addReview = function(req, res) {
@@ -19,7 +19,12 @@ var addReview = function(req, res) {
     Pros: req.body.Pros,
     Cons: req.body.Cons,
     CreatedBy: req.body.CreatedBy,
+    ReviewerFirstName: req.body.ReviewerFirstName,
+    ReviewerLastName: req.body.ReviewerLastName,
     CreatedFor: req.body.CreatedFor,
+    RevieweeFirstName: req.body.RevieweeFirstName,
+    RevieweeLastName: req.body.RevieweeLastName,
+    Type: req.body.Type,
     CreatedTime: Date.now(),
   });
   review.save(function(err, newReview) {
@@ -27,6 +32,10 @@ var addReview = function(req, res) {
       res.send(newReview);
     } else {
       res.sendStatus(400);
+      res.send({
+        error: err,
+        status: 400
+      });
     }
   });
 };
@@ -61,19 +70,45 @@ var updateReview = function(req, res) {
 };
 
 // Get only 10 review each request
-var getReviewOfUser = function(req, res) {};
+var getReviewOfUser = function(req, res) {
+  if (req.query.from === undefined || req.query.size === undefined || req.query.id === undefined){
+    res.sendStatus(400);
+    console.log("index, size, and user must be defined");
+  }
+  var index = parseInt(req.query.from);
+  var size = parseInt(req.query.size);
+  var user = mongoose.Types.ObjectId(req.query.id);
+  var query = {"$or": [{"CreatedBy": user}, {"CreatedFor": user}]};
+
+  Review.paginate(query, {offset: index, limit: size}, (err, result) => {
+    if (!err){
+      const asCreator = result.docs.filter(data => data.CreatedBy.toString() === req.query.id);
+      const asReviewee = result.docs.filter(data => data.CreatedFor.toString() === req.query.id);
+      console.log(asCreator);
+      console.log(asReviewee);
+      res.send({
+        asCreator: asCreator,
+        asReviewee: asReviewee,
+        total: result.totalDocs
+      })
+    } else {
+      console.log(err);
+      res.sendStatus({"error": "Feedback for user not found"})
+    }
+  })
+};
 
 // Function to get all the review created by user
-var getAllReviewOfUser = function(req, res) {
-  var createdById = req.body.CreatedBy;
-  Review.find({ CreatedBy: createdById }, function(err, reviews) {
-    if (!err) {
-      res.send(reviews);
-    } else {
-      res.sendStatus(404);
-    }
-  });
-};
+// var getAllReviewOfUser = function(req, res) {
+//   var createdById = req.body.CreatedBy;
+//   Review.find({ CreatedBy: createdById }, function(err, reviews) {
+//     if (!err) {
+//       res.send(reviews);
+//     } else {
+//       res.sendStatus(404);
+//     }
+//   });
+// };
 
 // Function to get all the review created for the user
 var getPersonalReviewOfUser = function(req, res) {
@@ -88,9 +123,10 @@ var getPersonalReviewOfUser = function(req, res) {
 };
 
 // Export the variable
-module.exports.getAllReview = getAllReview;
+// module.exports.getAllReview = getAllReview;
 module.exports.addReview = addReview;
 module.exports.deleteReview = deleteReview;
 module.exports.updateReview = updateReview;
-module.exports.getAllReviewOfUser = getAllReviewOfUser;
+// module.exports.getAllReviewOfUser = getAllReviewOfUser;
 module.exports.getPersonalReviewOfUser = getPersonalReviewOfUser;
+module.exports.getReviewOfUser = getReviewOfUser;
