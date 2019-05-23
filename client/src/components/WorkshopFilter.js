8f464409cbfb2f98c5a9d9e3d0a3883669505be0
MauriceBernardo <maurice_tikus@hotmail.com>
2019-05-23 22:02:28 +1000

    completed workshop filter & workshop search layout

--- client/src/components/WorkshopFilter.js
+++ client/src/components/WorkshopFilter.js
@@ -1,21 +1,75 @@
 import React, { Component } from "react";
-import Select from "react-select";
+import AsyncSelect from "react-select/lib/Async";
 import styles from "../styles/components/WorkshopFilter.module.css";
 
+// Workshop filter based on city and topic
+// Use react select async (https://react-select.com/async)
 class WorkshopFilter extends Component {
+  constructor(props) {
+    super(props);
+    this.fetchCity = this.fetchCity.bind(this);
+    this.fetchTopic = this.fetchTopic.bind(this);
+    this.handleCityChange = this.handleCityChange.bind(this);
+    this.handleTopicChange = this.handleTopicChange.bind(this);
+  }
+
+  handleCityChange = (value) => {
+    // value is object {value: "", label: ""}
+    this.props.handleCityFilter(value);
+  }
+
+  handleTopicChange = (value) => {
+    // value is object {value: "", label: ""}
+    this.props.handleTopicFilter(value);
+  }
+
+  fetchCity = async inputValue => {
+    // input value is a string (ex. "hello")
+    // fetch here then return the value of the label and value
+    await new Promise(resolve =>
+      setTimeout(() => {
+        console.log("yahoo");
+        resolve();
+      }, 5000)
+    );
+    return [{ value: inputValue, label: inputValue }];
+  };
+
+  fetchTopic = async inputValue => {
+    // input value is a string (ex. "hello")
+    // fetch here then return the value of the label and value
+    await new Promise(resolve =>
+      setTimeout(() => {
+        console.log("yahoo");
+        resolve();
+      }, 5000)
+    );
+    return [{ value: inputValue, label: inputValue }];
+  };
+
   render() {
     return (
       <div className={styles.drawer}>
-        <section className={styles.listing}>
-          <h1>Select category to filter:</h1>
-          <Select
-            className={styles.select}
-            classNamePrefix="select"
-            isClearable={true}
-            isSearchable={true}
-            name="category"
+        <div className={styles.citySelect}>
+          <h5>Filter by your city</h5>
+          <AsyncSelect
+            cacheOptions
+            value={this.props.filteredCity}
+            defaultOptions={[]}
+            loadOptions={this.fetchCity}
+            onChange={this.handleCityChange}
           />
-        </section>
+        </div>
+        <div className={styles.topicSelect}>
+          <h5>Filter by your topic</h5>
+          <AsyncSelect
+            cacheOptions
+            value={this.props.filteredTopic}
+            defaultOptions={[]}
+            loadOptions={this.fetchTopic}
+            onChange={this.handleTopicChange}
+          />
+        </div>
       </div>
     );
   }
--- client/src/pages/Home/WorkshopSearch.js
+++ client/src/pages/Home/WorkshopSearch.js
@@ -8,7 +8,7 @@ import { Redirect } from "react-router-dom";
 import WorkshopCardList from "../../components/WorkshopCardList";
 import WorkshopFilter from "../../components/WorkshopFilter";
 
-import styles from "../../styles/pages/Home/Search.module.css";
+import styles from "../../styles/pages/Home/WorkshopSearch.module.css";
 
 const mapStateToProps = state => {
   return {
@@ -20,40 +20,69 @@ class WorkshopSearch extends Component {
   constructor(props) {
     super(props);
     this.toggleDrawer = this.toggleDrawer.bind(this);
-    this.handleClick = this.handleClick.bind(this);
+    this.goToIndividuals = this.goToIndividuals.bind(this);
+    this.handleCityFilter = this.handleCityFilter.bind(this);
+    this.handleTopicFilter = this.handleTopicFilter.bind(this);
     this.state = {
       workshops: [
         {
-          name: "Cleaning house expert for dummies",
-          image: "",
-          category: "Cleaning",
+          name:
+            "Cleaning house expert for dummies everyone can do it let's go man oy yoy yo yo yo yo yo yo yo yo yo yo yo yo yo yo y y asdasd ad sadasdas",
+          date: "03/09/2019",
+          time: "16:00 - 18:00",
+          location: "321 Grattan Street, Melbourne",
           url: "https://www.github.com",
           _id: 1,
         },
+        {
+          name: "Cleaning house expert for dummies",
+          date: "03/09/2019",
+          time: "16:00 - 18:00",
+          location: "321 Grattan Street, Melbourne",
+          url: "https://www.github.com",
+          _id: 2,
+        },
       ],
+      // should be in [{value : "" , label: ""}] format
+      filteredCity: [],
+      // should be in [{value : "" , label: ""}] format
+      filteredTopic: [],
       drawerOpen: false,
       pageNumber: 1,
       totalPageNumber: 1,
-      dataPerPage: 8,
+      dataPerPage: 4,
       currentIndex: 0,
       totalItem: 0,
-      filterSkill: [],
-      skills: [],
       changes: false,
-      hasSearcher: 0,
       individual: false,
     };
   }
 
-  //   componentDidMount = async () => {};
+  componentDidMount = async () => {};
 
-  //   componentDidUpdate = async () => {
-  //     if (this.state.changes) {
-  //       this.setState({
-  //         changes: false
-  //       });
-  //     }
-  //   };
+  componentDidUpdate = async () => {
+    if (this.state.changes) {
+      // call fetch data function here
+
+      this.setState({
+        changes: false
+      });
+    }
+  };
+
+  handleCityFilter = (city) => {
+    this.setState({
+      filteredCity: [city],
+      changes: true,
+    })
+  }
+
+  handleTopicFilter = (topic) => {
+    this.setState({
+      filteredTopic: [topic],
+      changes: true,
+    })
+  }
 
   toggleDrawer = open => () => {
     this.setState({
@@ -61,7 +90,11 @@ class WorkshopSearch extends Component {
     });
   };
 
-  handleClick = id => {};
+  goToIndividuals = () => {
+    this.setState({
+      individual: true,
+    });
+  };
 
   //   handleNextPage = () => {
   //     if (this.state.pageNumber < this.state.totalPageNumber) {
@@ -82,12 +115,6 @@ class WorkshopSearch extends Component {
   //         });
   //       }
   //   };
-
-  goToIndividuals = () => {
-    this.setState({
-      individual: true,
-    });
-  };
 
   render() {
     if (this.state.individual === true) {
@@ -126,9 +153,14 @@ class WorkshopSearch extends Component {
           open={this.state.drawerOpen}
           onClose={this.toggleDrawer(false)}
         >
-          <WorkshopFilter />
+          <WorkshopFilter
+            filteredCity={this.state.filteredCity}
+            filteredTopic={this.state.filteredTopic}
+            handleCityFilter={this.handleCityFilter}
+            handleTopicFilter={this.handleTopicFilter}
+          />
         </Drawer>
-        <div className={styles["card-container"]}>
+        <div>
           <WorkshopCardList values={this.state.workshops} />
         </div>
         <div className={styles.bottom}>
